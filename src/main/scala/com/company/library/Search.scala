d01c4989a361c672f884e7decccd875dc1333acb
Tom Miller <thomas.j.c.miller@outlook.com>
2019-04-07 12:47:54 +0100

    new method bookByAuthor

---
+++ src/main/scala/com/company/library/Search.scala
@@ -0,0 +1,10 @@
+package com.company.library
+
+class Search {
+  var books: List[Book] = com.company.library.Books.all
+
+  def bookByTitle(title: String): Boolean = books.exists(_.title == title)
+
+  def bookByAuthor(author: String): Boolean = books.exists(_.author == author)
+
+}
---
+++ src/test/scala/com/company/library/SearchSpec.scala
@@ -0,0 +1,16 @@
+package com.company.library
+
+import org.scalatest.FunSuite
+import org.scalatest.Matchers
+
+class SearchSpec extends FunSuite with Matchers {
+  val newSearch = new Search()
+
+  test(testName = "search bookByTitle") {
+    newSearch.bookByTitle("New Moon") shouldBe true
+  }
+
+  test(testName = "search bookByAuthor") {
+    newSearch.bookByAuthor("Oliver, Jamie") shouldBe true
+  }
+}
