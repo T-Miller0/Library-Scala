d2d1cf2e4853db0c627b9e0df1c1a5e0579f62d9
Tom Miller <thomas.j.c.miller@outlook.com>
2019-04-07 12:46:53 +0100

    Created new class search and moved bookByTitle method into the class

--- src/main/scala/com/company/library/Library.scala
+++ src/main/scala/com/company/library/Library.scala
@@ -3,9 +3,4 @@ package com.company.library
 //object Library {
 
   class Library {
-    var books: List[Book] = com.company.library.Books.all
-
-    def bookByTitle(title: String): Boolean = books.exists(_.title == title)
-  }
-
-//}
+}
--- src/test/scala/com/company/library/LibrarySpec.scala
+++ src/test/scala/com/company/library/LibrarySpec.scala
@@ -2,16 +2,11 @@ package com.company.library
 
 import org.scalatest.FunSuite
 import org.scalatest.Matchers
-import com.company.library._
 
 class LibrarySpec extends FunSuite with Matchers {
   val myLibrary = new Library()
 
   test("some words to describe my test") {
     "aString" shouldBe "aString"
-  }
-
-  test(testName = "search bookByTitle") {
-    myLibrary.bookByTitle("New Moon") shouldBe true
   }
 }
